6bdfc205281d0f5697e417b5d6f3e1741de8b9d8
Russkih Anatoly <titanofdarkness@gmail.com>
2019-08-07 20:55:39 +1000

    improve env overriding.

--- env-files/index.js
+++ env-files/index.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const fs = require('fs')
+const dotenv = require('dotenv')
 
 const fileName = `./${process.env.NODE_ENV || 'development'}.env`;
 const fullPath = path.resolve(__dirname, fileName);
-let env = require('dotenv').config({
+const env = dotenv.config({
   path: fullPath,
 });
 const logger = require('../logger');
@@ -14,12 +16,14 @@ if (env.error) {
 const deployedFileName = '.env';
 const fullDeployedPath = path.resolve(__dirname, '..', deployedFileName);
 
-env = require('dotenv').config({
-  path: fullDeployedPath,
-});
-
-if (env.error) {
-  if (env.error.code === 'ENOENT') {
+try {
+  const envConfig = dotenv.parse(fs.readFileSync(fullDeployedPath));
+  // eslint-disable-next-line guard-for-in,no-restricted-syntax
+  for (const k in envConfig) {
+    process.env[k] = envConfig[k];
+  }
+} catch (e) {
+  if (e.code === 'ENOENT') {
     logger.warn('Deployed .env file is missing, skipping.');
   } else {
     logger.error('Error loading enviroment files: %s', env.error);
